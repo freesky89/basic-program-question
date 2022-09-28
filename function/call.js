a165ac548484f89ada6238cb724ea16900baede1
zidongping <529309498@qq.com>
2022-09-29 00:04:42 +0800

    feat: add simulate call method

---
+++ function/call.js
@@ -0,0 +1,14 @@
+// implement the myCall to simulate the fn.call(obj, ...args);
+// notice: we cannot use es6 method
+Function.prototype.myCall = function(context = window) {
+    // this equals the function which execute the myCall method;
+    context.fn = this;
+    // collect the arguments
+    const argumentsArr = Array.prototype.slice.call(arguments).slice(1);
+    // with the eval method to simulate parameter tranfer
+    const result = eval('context.fn(' + argumentsArr+ ')');
+    // delete the extra property
+    delete context.fn;
+    return result;
+}
+
